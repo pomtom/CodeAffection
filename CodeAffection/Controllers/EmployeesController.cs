6a9598f503c66d7049fb13abfc4423826aa63ebc
Pramod Lawate <pramod.lawate@btl.com>
2023-02-03 00:10:35 +0000

    Added common method to create and update employee

--- CodeAffection/Controllers/EmployeesController.cs
+++ CodeAffection/Controllers/EmployeesController.cs
@@ -6,98 +6,72 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using CodeAffection.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace CodeAffection.Controllers
 {
     public class EmployeesController : Controller
     {
         private readonly EmployeeDbContext _context;
+        private readonly IConfiguration _configuration;
 
-        public EmployeesController(EmployeeDbContext context)
+        public EmployeesController(EmployeeDbContext context, IConfiguration configuration)
         {
             _context = context;
+            this._configuration = configuration;
         }
 
         // GET: Employees
         public async Task<IActionResult> Index()
         {
-              return View(await _context.Employees.ToListAsync());
+            return View(await _context.Employees.ToListAsync());
         }
 
-        // GET: Employees/Details/5
-        public async Task<IActionResult> Details(int? id)
+        // GET: Employees/AddOrEdit/5
+        public async Task<IActionResult> AddOrEdit(int id = 0)
         {
-            if (id == null || _context.Employees == null)
+
+            if (id == 0)
             {
-                return NotFound();
+                return View(new Employee());
+            }
+            else
+            {
+                if (_context.Employees == null)
+                {
+                    return NotFound();
+                }
+
+                var employee = await _context.Employees.FindAsync(id);
+                if (employee == null)
+                {
+                    return NotFound();
+                }
+                return View(employee);
             }
 
-            var employee = await _context.Employees
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (employee == null)
-            {
-                return NotFound();
-            }
-
-            return View(employee);
         }
 
-        // GET: Employees/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Employees/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // POST: Employees/AddOrEdit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,City,Phone,BirthDate")] Employee employee)
+        public async Task<IActionResult> AddOrEdit(int id, [Bind("Id,Name,City,Phone,BirthDate")] Employee employee)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Add(employee);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(employee);
-        }
-
-        // GET: Employees/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Employees == null)
-            {
-                return NotFound();
-            }
-
-            var employee = await _context.Employees.FindAsync(id);
-            if (employee == null)
-            {
-                return NotFound();
-            }
-            return View(employee);
-        }
-
-        // POST: Employees/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,City,Phone,BirthDate")] Employee employee)
-        {
-            if (id != employee.Id)
-            {
-                return NotFound();
-            }
-
             if (ModelState.IsValid)
             {
                 try
                 {
-                    _context.Update(employee);
-                    await _context.SaveChangesAsync();
+                    if (employee.Id == 0)
+                    {
+                        _context.Add(employee);
+                        await _context.SaveChangesAsync();
+                        return RedirectToAction(nameof(Index));
+                    }
+                    else
+                    {
+                        _context.Update(employee);
+                        await _context.SaveChangesAsync();
+                    }
                 }
                 catch (DbUpdateConcurrencyException)
                 {
@@ -115,23 +89,6 @@ namespace CodeAffection.Controllers
             return View(employee);
         }
 
-        // GET: Employees/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Employees == null)
-            {
-                return NotFound();
-            }
-
-            var employee = await _context.Employees
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (employee == null)
-            {
-                return NotFound();
-            }
-
-            return View(employee);
-        }
 
         // POST: Employees/Delete/5
         [HttpPost, ActionName("Delete")]
@@ -147,14 +104,14 @@ namespace CodeAffection.Controllers
             {
                 _context.Employees.Remove(employee);
             }
-            
+
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
         private bool EmployeeExists(int id)
         {
-          return _context.Employees.Any(e => e.Id == id);
+            return _context.Employees.Any(e => e.Id == id);
         }
     }
 }
